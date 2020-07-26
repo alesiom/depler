c5f8d943a20b1440aa200054c355a0fbee379be8
Alexandre Margot <alesiom@Alexandres-MacBook-Pro-4.local>
2020-07-26 20:29:00 +0200

    relink

--- src/js/cms.js
+++ src/js/cms.js
@@ -5,15 +5,13 @@ import CMS from "netlify-cms-app";
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
 import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
+import ProductsPreview from "./cms-preview-templates/dienstleistungen";
+import ValuesPreview from "./cms-preview-templates/ueber-uns";
+import ContactPreview from "./cms-preview-templates/kontakt";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("dienstleistungen", ProductsPreview);
+CMS.registerPreviewTemplate("ueber-uns", ValuesPreview);
+CMS.registerPreviewTemplate("kontakt", ContactPreview);
 CMS.init();
